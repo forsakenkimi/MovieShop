97c2d02f1e87fce8fed3330c68f3e67a6a3bb18f
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-09 09:56:00 -0700

    Irepository created

---
+++ ApplicationCore/Contracts/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        //This is a base interface with basic CRUD functionalities
+
+        //Get record by id
+        T GetById(int id);
+        
+        //Getting all the records; usually for small table
+        IEnumerable<T> GetAll();
+
+        T Add(T entity);
+        
+        T Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
