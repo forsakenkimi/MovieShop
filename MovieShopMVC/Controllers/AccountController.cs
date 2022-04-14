980a3db5556e72f7e36339aac18c8daee6ea3468
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-14 11:45:06 -0700

    added form for account/login & /register

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(RegisterModel model)
         {
             return View();
         }
