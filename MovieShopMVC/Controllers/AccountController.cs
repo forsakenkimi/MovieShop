567846d7853ae4e557813610048c8e9612653499
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-14 15:31:07 -0700

    registration done

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,28 +1,39 @@
-﻿using ApplicationCore.Models;
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
+
     {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        [HttpGet]
         public async Task<IActionResult> Login()
         {
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
             return View();
         }
 
+        [HttpGet]
         public async Task<IActionResult> Register()
         {
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> Register(RegisterModel model)
         {
-            return View();
+            bool user = await _accountService.RegisterUser(model);
+            return RedirectToAction("Login");
         }
     }
 }
