381e943dddb0795b1ea836d62dbb62bd9ebbfa84
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-17 14:21:53 -0700

    before assignment

--- MovieShopMVC/Controllers/UserController.cs
+++ MovieShopMVC/Controllers/UserController.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MovieShopMVC.Controllers
 {
+    [Authorize]
+
     public class UserController : Controller
     {
+        //created by myself
+        private int GetUserId() {
+            var userId = Convert.ToInt32(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            return userId;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Purchases()
         {
+            int userId = GetUserId();
+            System.Diagnostics.Debug.WriteLine(userId);
             return View();
         }
 
         [HttpGet]
         public async Task<IActionResult> Favorites()
         {
+            int userId = GetUserId();
             return View();
         }
     }
