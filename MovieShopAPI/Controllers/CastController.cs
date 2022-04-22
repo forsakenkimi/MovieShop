f74752b7d5192b087731ebe3f375fd54fad39f3a
forsakenkimi <kckcheng@ucdavis.edu>
2022-04-22 02:28:07 -0700

    cast done

--- MovieShopAPI/Controllers/CastController.cs
+++ MovieShopAPI/Controllers/CastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopAPI.Controllers
@@ -7,5 +8,21 @@ namespace MovieShopAPI.Controllers
     [ApiController]
     public class CastController : ControllerBase
     {
+        private readonly ICastService _castService;
+        public CastController(ICastService castService)
+        {
+            _castService = castService;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var castDetails = await _castService.GetCastDetails(id);
+            if (castDetails == null)
+            {
+                return NotFound(new { errorMessage = "No Cast Found" });
+            }
+            return Ok(castDetails);
+        }
     }
 }
